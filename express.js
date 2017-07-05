0d217628c3a63e03b10903607b346c26a4b7e088
Shannon Feng <16shannon.feng@gmail.com>
2017-07-05 16:40:14 -0500

    New repository WebApp2

---
+++ express.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var mysql = require('mysql');
+var favicon = require('serve-favicon');
+var path = require('path');
+var app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));  // Use favicon.ico
+app.use(express.static(path.join(__dirname, '/')));   // Use this directory (C://WebApp2/)
+app.use(express.static(path.join(__dirname, 'public')));  // Use the /public folder
+
+var server = app.listen(5000, function () {
+    console.log('Node server is running on http://localhost:5000...');
+});
+
+// Establish connection to acronym_import database
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "Shannon",
+  password: "YOLOmoney$$$444",
+  database: "acronym_import"
+});
+
+// Connect to database
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Successfully connected to acronym database.");
+});
+
+// Define routes here...
+/* Homepage */
+app.get('/', function (req, res)
+{
+  return res.sendFile('C://WebApp2/index.html');
+});
+
+app.get('/acronym/:acr', function(req, res)
+{
+  console.log(req.params.acr);
+  return res.end();
+});
+
+/* About page */
+app.get('/about', function (req, res)
+{
+  return res.sendFile('C://WebApp2/about.html');
+});
+
+/* Privacy policy page */
+app.get('/privacypolicy', function (req, res)
+{
+  return res.sendFile('C://WebApp2/privacypolicy.html');
+});
+
+/* Contact page */
+app.get('/contact', function (req, res)
+{
+  return res.sendFile('C://WebApp2/contact.html');
+});
+
+app.post('/add/:acronym/:def/:comment', function(req, res)
+{
+  var acronym = req.params.acronym;
+  var def = req.params.def;
+  var comment = req.params.comment;
+  var sql = "INSERT INTO acronym_table (acronym, definition, cmt) VALUES ('" +acronym+ "', '" +def+ "', '" +comment+ "')";
+  con.query(sql, function(err, result, fields)
+  {
+    if (err) throw err;
+    res.end();
+  });
+});
+
+app.post('/query/:string', function(req, res)
+{
+  var string = req.params.string + "%";   // Need to append '%' sign to generate all possible results
+  var sql = "SELECT * FROM acronym_table WHERE acronym LIKE '" +string+ "' ORDER BY acronym";
+  var jsObj = {   //javascript object that will be converted to JSON text when response is sent
+    acronym: [],
+    definition: [],
+    comment: []
+  };
+
+  con.query(sql, function(err, result, fields)
+  {
+    if (err) throw err;
+    if (result.length == 0)
+    {
+      res.json(-1);
+    }
+    else
+    {
+      for (var i = 0; i < result.length; i++)
+      {
+        var acronymToAdd = result[i].acronym;
+        var defToAdd = result[i].definition;
+        jsObj.acronym.push(acronymToAdd);
+        jsObj.definition.push(defToAdd);
+        var commentToAdd = result[i].cmt;
+        if (commentToAdd == null)
+          commentToAdd = "No comment";
+        jsObj.comment.push(commentToAdd);
+      }
+      res.json(jsObj);  //.json method converts the javascript object to a JSON string and sends it as response
+    }
+  });
+});
---
+++ public/javascript_functions.js
@@ -0,0 +1,114 @@
+var app = angular.module("acronymApp", ["ngSanitize"]);
+
+/* Executed when user clicks the "Add!" button on add acronym form */
+app.controller("addAcronymCtrl", function($scope, $http)
+{
+  $scope.add = function add()
+  {
+    var abbrev = document.getElementById("abbrev");
+    var def = document.getElementById("def");
+    var comment = document.getElementById("comment");
+    // Check if abbreviation or definition fields are blank
+    if (abbrev.value == "")
+    {
+      // Launch modal to alert user to add abbreviation
+      $(document).ready(function()
+      {
+          $("#abbrevAlert").modal();
+      });
+      return;
+    }
+    if (def.value == "")
+    {
+      // Launch modal to alert user to add definition
+      $(document).ready(function()
+      {
+          $("#defAlert").modal();
+      });
+      return;
+    }
+    if (comment.value == "")
+    {
+      // if no comment, set value to null
+      comment.value = null;
+    }
+    var req = new XMLHttpRequest();
+    req.open("POST","/add/"+abbrev.value+"/"+def.value+"/"+comment.value, true);
+    req.onreadystatechange = pollStateChange;   // when server response is ready, call the function
+    function pollStateChange()
+    {
+      if (this.readyState == 4 && this.status == 200)
+      {
+        // Launch success modal to indicate acronym added
+        $(document).ready(function()
+        {
+            $("#successModal").modal();
+        });
+      }
+      else
+      {
+        console.log("Error", req.statusText);
+      }
+    }
+    req.send(null); // send request to searchDatabase.php file
+  }
+});
+
+/* Executed when user types into search box */
+app.controller("searchCtrl", function($scope, $http)
+{
+  $scope.records = [];
+  $scope.infoPage = function infoPage(x)
+  {
+    var req = new XMLHttpRequest();
+    console.log(x);
+    //req.open("POST","/acronym/"+data, true);
+  };
+
+  $scope.search = function search()
+  {
+    var str = document.getElementById("searchInput").value;
+    var req = new XMLHttpRequest();
+    var results = "";
+    if (str.length == 0)
+    {
+      $scope.setDisplay = false;
+      $scope.text1 = false;
+      $scope.text2 = false;
+    }
+    else
+    {
+      req.open("POST","/query/"+str, true);
+      req.onreadystatechange = pollStateChange;   // when server response is ready, call the function
+      function pollStateChange()
+      {
+        if (this.readyState == 4 && this.status == 200)
+        {
+          var jsonResults = JSON.parse(req.responseText); // parses the JSON text into javascript object
+          if (jsonResults == -1)
+          {
+            results = "No results";
+          }
+          else
+          {
+            for (i in jsonResults.acronym)
+            {
+              $scope.records.push(jsonResults.acronym[i] + ": " + jsonResults.definition[i] + "  " + jsonResults.comment[i]);
+              results += jsonResults.acronym[i] + ": " + jsonResults.definition[i] + "  " + jsonResults.comment[i] + "\n\n";
+            }
+          }
+          $scope.results = results;
+          $scope.setDisplay = true;
+          $scope.text1 = true;
+          $scope.text2 = true;
+          $scope.$apply();
+        }
+        else
+        {
+          console.log("Error", req.statusText);
+        }
+      }
+      req.send(null);
+    }
+  }
+});
